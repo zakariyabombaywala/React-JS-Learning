c6dd8587f4fc656f499cd1d73c64b36382b70c6f
zakariyabombaywala <zakariyabombaywala@gmail.com>
2024-02-14 11:36:52 +0530

    React JS - Tailwind with Props

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Card from "./components/card";
 
 function App() {
   return (
     <>
-      <h1 className="bg-green-500 text-black">Tailwind with React</h1>
+      <h1 className="bg-green-500 text-black mb-5 rounded-xl p-10">
+        Tailwind with React
+      </h1>
+      <Card name="React" btnText="Click me" />
     </>
   );
 }
