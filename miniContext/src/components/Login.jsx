c88cdffc843f0a75f3da9ab4963a889709639c80
zakariyabombaywala <zakariyabombaywala@gmail.com>
2024-02-26 12:22:58 +0530

    Project 06 : Context API understandig mini project

---
+++ miniContext/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(UserContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        type="text"
+        placeholder="username"
+      />{" "}
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="text"
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
